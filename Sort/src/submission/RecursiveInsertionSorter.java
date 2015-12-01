d082222b2d492e0da8a8008b26858d7153aeb4f3
emily <emily.m.hill@gmail.com>
2015-12-01 15:07:57 -0500

    clarified sort recursion

--- Sort/src/submission/RecursiveInsertionSorter.java
+++ Sort/src/submission/RecursiveInsertionSorter.java
@@ -2,6 +2,10 @@ package submission;
 
 public class RecursiveInsertionSorter {
 	public static int[] sort(int[] unsorted) {
+		return sort(unsorted, 0); // Don't touch
+	}
+
+	public static int[] sort(int[] unsorted, int index) {
 		// TODO implement recursive insertion sort
 		return null;
 	}
