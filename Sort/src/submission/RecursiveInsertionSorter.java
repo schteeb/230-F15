d6d52f3a5b6611fd17b4f7fe193f48e758ba945f
emily <emily.m.hill@gmail.com>
2015-12-01 15:19:02 -0500

    fixed length - 1

--- Sort/src/submission/RecursiveInsertionSorter.java
+++ Sort/src/submission/RecursiveInsertionSorter.java
@@ -2,7 +2,7 @@ package submission;
 
 public class RecursiveInsertionSorter {
 	public static int[] sort(int[] unsorted) {
-		return sort(unsorted, unsorted.length); // Don't touch
+		return sort(unsorted, unsorted.length-1); // Don't touch
 	}
 
 	private static int[] sort(int[] unsorted, int endIndex) {
